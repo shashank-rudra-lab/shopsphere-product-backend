53d8c6c526fcdd5633547a5c0d381e8420c2dece
sairudra <sairudra@geotab.com>
2025-06-11 13:43:23 -0400

    Initial commit - Product-backend

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, jsonify
+from google.cloud import datastore
+import os
+
+app = Flask(__name__)
+
+# Set up Google Cloud credentials (ensure this path is correct)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-account.json"
+datastore_client = datastore.Client()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/add-product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        image_link = request.form.get('image_link')
+        product_name = request.form.get('product_name')
+        price = request.form.get('price')
+
+        # Create Datastore key
+        key = datastore_client.key('Product', product_id)     
+        entity = datastore.Entity(key=key)
+        entity.update({
+            'image_url': image_link,
+            'name': product_name,
+            'price': price
+        })
+
+        datastore_client.put(entity)
+        return render_template('submitted.html')
+    return render_template('add_product.html')
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    query = datastore_client.query(kind='Product')
+    products = list(query.fetch())
+    result = []
+    for product in products:
+        item = {
+            'product_id': product.key.name,
+            'image_url': product.get('image_url'),
+            'name': product.get('name'),
+            'price': product.get('price')
+        }
+        result.append(item)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
